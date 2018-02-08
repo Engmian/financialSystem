1b8a8d933f9f4d002083be3a1a006264f793c7cc
Rabby Wales <35256019+Hinanawi@users.noreply.github.com>
2018-02-08 13:48:29 +0800

    轮播图实体类

---
+++ financial/src/com/financial/entity/Roll_pic.java
@@ -0,0 +1,40 @@
+package com.financial.entity;
+
+public class Roll_pic {
+    private int pid;
+    private String rp_name;
+    private String rp_desc;
+    private String rp_url;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getRp_name() {
+        return rp_name;
+    }
+
+    public void setRp_name(String rp_name) {
+        this.rp_name = rp_name;
+    }
+
+    public String getRp_desc() {
+        return rp_desc;
+    }
+
+    public void setRp_desc(String rp_desc) {
+        this.rp_desc = rp_desc;
+    }
+
+    public String getRp_url() {
+        return rp_url;
+    }
+
+    public void setRp_url(String rp_url) {
+        this.rp_url = rp_url;
+    }
+}
