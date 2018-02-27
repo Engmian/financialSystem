61280e1d41c65afa672fcb9740ffe7a39726488a
Rabby Wales <35256019+Hinanawi@users.noreply.github.com>
2018-02-27 15:18:03 +0800

    ???

--- financial/src/com/financial/test/Test.java
+++ financial/src/com/financial/test/Test.java
@@ -1,6 +1,7 @@
 package com.financial.test;
 
-import com.financial.entity.User;
+import com.financial.entity.Product;
+import com.financial.service.ProductService;
 import com.financial.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,13 +14,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         ApplicationContext act = new ClassPathXmlApplicationContext("applicationContext-mybatis.xml");
-        UserService userService = (UserService) act.getBean("userServiceImpl");
-        /*User useruphone = userService.findUseruphone("13546270677");*/
-        int userCreate=-1;
-        userCreate = userService.createUser("18588450732", "456789");
+        /*UserService userService =(UserService) act.getBean("userServiceImpl");
+        String useruphone = userService.findUseruphone("18588450732");
 
-            System.out.println(userCreate);
+        System.out.println(useruphone);*/
 
-
+        ProductService productService = (ProductService)act.getBean("productServiceImpl");
+        Product noviceType = productService.findNoviceType(1);
+        System.out.println(noviceType.getPro_day());
     }
 }
